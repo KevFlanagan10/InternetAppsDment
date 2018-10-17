355221eb6c0ce18731a17631e904dd85d06b7227
KevFlanagan <G00228079@gmit.ie>
2018-10-17 09:23:53 +0100

    Add project files.

---
+++ Puzzle Pic'z/Puzzle Pic'z/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Puzzle_Pic_z
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
