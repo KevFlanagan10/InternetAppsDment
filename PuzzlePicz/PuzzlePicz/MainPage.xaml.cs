cd7d3f5a1779ed30979141ad4ac83e0969de95ab
KevFlanagan <G00228079@gmit.ie>
2018-11-21 10:44:36 +0000

    Add project files.

---
+++ PuzzlePicz/PuzzlePicz/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PuzzlePicz
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            setUpImage();
+        }
+
+        private void setUpImage()
+        {
+            
+            var assemble = typeof(MainPage);
+
+            string filename = "PuzzlePicz.Assets.Images.Picz.PNG";
+
+            MainImage.Source = ImageSource.FromResource(filename , assemble);
+
+         
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string text = MainEntry.Text;
+
+            MainLabel.Text = "Hello " + text;
+
+ 
+        }
+
+    }
+}
